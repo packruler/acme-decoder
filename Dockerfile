d6dea7ad6d7d4a475c1e764c48527573fd2bbdba
Ethan Leisinger <packruler@gmail.com>
2023-01-08 19:32:52 -0700

    Add support for automatically setting permissions
    Permissions of created files and dirs match acme.json permissions

--- Dockerfile
+++ Dockerfile
@@ -2,9 +2,9 @@ FROM alpine:3.17
 
 RUN apk --no-cache add jq
 
-COPY --chown=root:root ./acme_decoder.sh /bin/acme-decode
-RUN chmod 777 /bin/acme-decode
+COPY --chown=root:root ./acme-exporter.sh /bin/acme-exporter
+RUN chmod 777 /bin/acme-exporter
 
 WORKDIR /data
 
-ENTRYPOINT [ "/bin/acme-decode", "./acme.json" ]
+ENTRYPOINT [ "/bin/acme-exporter", "./acme.json" ]
---
+++ acme-exporter.sh
@@ -0,0 +1,42 @@
+#! /bin/sh
+
+USER=$(stat -c '%U' acme.json)
+GROUP=$(stat -c '%G' acme.json)
+
+file_path=$1
+
+
+if [ ! -d certs ]; then
+    mkdir certs
+    chown "${USER}:${GROUP}" certs
+fi
+if [ ! -d priv ]; then
+    mkdir priv
+    chown "${USER}:${GROUP}" priv
+fi
+
+for row in $(cat $file_path | jq ".[\"lets-encrypt\"].Certificates" | jq -r ".[] | @base64"); do
+    row=$(echo ${row} | base64 -d)
+
+    domain=$(echo $row | jq -r ".domain.main")
+    echo "Exporting $domain"
+    
+    cert=$(echo $row | jq -r ".certificate" | base64 -d)
+    key=$(echo $row | jq -r ".key" | base64 -d)
+
+    cert_file="certs/${domain}.crt"
+    if [ ! -f $cert_file ]; then
+        touch $cert_file
+        chown "${USER}:${GROUP}" $cert_file
+    fi
+
+    key_file="priv/${domain}.key"
+    if [ ! -f $cert_file ]; then
+        touch $cert_file
+        chown "${USER}:${GROUP}" $cert_file
+    fi
+
+    echo "$cert" > $cert_file
+    echo "$key" > $key_file
+    echo "Done."
+done
